52c080c90702a5eeca7be3d4fb57d0e076dc82f9
kalitka <zavorotnii.dr@gmail.com>
2024-03-20 12:30:09 +0700

    Structure getter methods

--- integrations/soundcloud/utils.py
+++ integrations/soundcloud/utils.py
@@ -1,9 +1,23 @@
 from integrations.conf import BASE_DIR
 
 
+TRACK_FILETYPE = "mp3"
+NAME_SEPARATOR = " - "
+
+
 def get_track_filename(featured_track):
-    return f"{BASE_DIR}/saved/{featured_track.artist} - {featured_track.title}.mp3"
+    return (
+        f"{BASE_DIR}/saved/{featured_track.artist}"
+        f"{NAME_SEPARATOR}"
+        f"{featured_track.title}"
+        f".{TRACK_FILETYPE}"
+    )
 
 
 def get_playlist_filename(featured_playlist):
-    return f"./saved/{featured_playlist.artist} -" f" {featured_playlist.title}.mp3"
+    return (
+        f"{BASE_DIR}/saved/{featured_playlist.artist} "
+        f"{NAME_SEPARATOR}"
+        f"{featured_playlist.title}"
+        f".{TRACK_FILETYPE}"
+    )
