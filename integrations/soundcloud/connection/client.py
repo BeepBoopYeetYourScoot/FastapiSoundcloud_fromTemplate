a8c9507eb812ed1b280e2655fb9192a72b5ec46c
kalitka <zavorotnii.dr@gmail.com>
2024-03-20 12:30:54 +0700

    Remove unused SC script

--- integrations/soundcloud/connection/client.py
+++
@@ -1,6 +0,0 @@
-import soundcloud
-
-# As per https://stackoverflow.com/questions/54753238/soundcloud-application-registration-form-is-closed
-# Registering app is not required by Soundcloud
-TOKEN = ...
-client = soundcloud.client.Client(access_token=...)
