3b2c8fac8a25261c7b31ba45b1c740f3cf822bf1
kalitka <zavorotnii.dr@gmail.com>
2024-03-21 11:08:01 +0700

    Move segmentation scripts to segmentation dir

--- integrations/soundcloud/segmentation/segment.py
+++ integrations/soundcloud/segmentation/segment.py
@@ -1,10 +1,12 @@
+from typing import Tuple
+
 import pydub
 
 from integrations.conf import TRACK_FILETYPE
-from integrations.soundcloud.saving.saved.tracks.segmented.scripts.builders import (
+from integrations.soundcloud.segmentation.scripts.builders import (
     build_segmented_track_path,
 )
-from integrations.soundcloud.saving.saved.tracks.segmented.scripts.getters import (
+from integrations.soundcloud.segmentation.scripts.getters import (
     get_original_track_by_name,
 )
 
@@ -13,10 +15,21 @@ TRACK_NAME = "soft blade - Ksv.mp3"
 track = get_original_track_by_name(TRACK_NAME)
 
 
-def split_in_half(track_name): ...
+def split_track_in_two_halves(
+    track: pydub.AudioSegment,
+) -> Tuple[pydub.AudioSegment, pydub.AudioSegment]:
+    halving_point = len(track) / 2
+    first_half = track[:halving_point]
+    second_half = track[halving_point:]
+    return first_half, second_half
 
 
-def cut_by_start_and_end(): ...
+def cut_by_start_and_end(
+    track: pydub.AudioSegment, start_ms=0, end_ms=None
+) -> pydub.AudioSegment:
+    starting_track_point = track[start_ms:]
+    ending_track_point = track[:end_ms] if end_ms else ...
+    return track
 
 
 def save_changed_track(changed_track: pydub.AudioSegment):
