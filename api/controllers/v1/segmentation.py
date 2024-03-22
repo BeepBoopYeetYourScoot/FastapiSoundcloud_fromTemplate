f63369bd9e41b755fa23f9b1dd72b1ed53b8959b
kalitka <zavorotnii.dr@gmail.com>
2024-03-22 13:04:05 +0700

    Add class-based controller

--- api/controllers/v1/segmentation.py
+++ api/controllers/v1/segmentation.py
@@ -1,14 +1,27 @@
 import uvicorn
 from fastapi import FastAPI, Response, status
-from fastapi.websockets import WebSocket
+from fastapi_controllers import Controller, get
 
-from fastapi_controllers import Controller, get, websocket
+from api.schemas.v1.segmentation.splitter import SplitterRequestData
+from integrations.soundcloud.segmentation.scripts.builders import build_full_track_name
+from integrations.soundcloud.segmentation.scripts.getters import (
+    get_original_track_by_name,
+)
+from integrations.soundcloud.segmentation.scripts.segment import (
+    split_track_in_two_halves,
+)
 
 
 class TrackSplitter(Controller):
+    prefix = "/splitter"
+    tags = ["splitter"]
+
     @get("/example", response_class=Response)
-    async def example(self) -> Response:
-        return Response("Successful response", status_code=status.HTTP_200_OK)
+    async def split_track_into_two_parts(self, data: SplitterRequestData) -> Response:
+        full_track_name = build_full_track_name(data.track_uuid, data.track_name)
+        track = get_original_track_by_name(full_track_name)
+        first, second = split_track_in_two_halves(track)
+        return Response([first, second], status_code=status.HTTP_200_OK)
 
 
 if __name__ == "__main__":
