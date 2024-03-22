ae65c1e984b637fa6b7fda094973e1c3579e6238
kalitka <zavorotnii.dr@gmail.com>
2024-03-22 13:03:21 +0700

    Add builder method

--- integrations/soundcloud/segmentation/scripts/builders.py
+++ integrations/soundcloud/segmentation/scripts/builders.py
@@ -5,3 +5,7 @@ from integrations.conf import TRACK_SAVED_SEGMENTED
 
 def build_segmented_track_path(track_name: str):
     return TRACK_SAVED_SEGMENTED / Path(track_name)
+
+
+def build_full_track_name(uu_id: str, name: str):
+    return f"{uu_id}:{name}"
